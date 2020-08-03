c25f7d7dee40ea1e42a01f157b1cb8134040469d
nanthan75 <thayananthan@gmail.com>
2020-08-03 11:24:45 +0100

    pie chart from input given and variable of variables(hahaha...) not finished.... plot works

---
+++ Pie chart from input.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plot
+
+# is it possible to ask how many variables, then according to that, give input for that number of variables?
+a = input_str=input('input variables in the following way\n name1,value1,name2,value2,....')
+a.split(',')
+
+figureObject, axesObject = plot.subplots()
+# Draw the pie chart
+axesObject.pie(a,
+               labels=a,
+               autopct='%1.2f',
+               startangle=90)
+axesObject.axis('equal')
+
+plot.show()
