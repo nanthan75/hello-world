a8741af45ac53a0da3232ae15ab32506a7f2d01a
nanthan75 <thayananthan@gmail.com>
2020-08-04 08:25:22 +0100

    border turtle. chaange the numbers to have differentshapes and sizes

---
+++ Python test border.py
@@ -0,0 +1,18 @@
+import turtle
+import time
+
+bor = turtle.Turtle()
+bor.hideturtle()
+bor.penup()
+bor.goto(-200, 0)
+bor.pendown()
+bor.left(90)
+bor.forward(200)
+bor.right(90)
+bor.forward(400)
+bor.right(90)
+bor.forward(400)
+bor.right(90)
+bor.forward(400)
+bor.right(90)
+bor.forward(200)
